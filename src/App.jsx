132bc39403db4e60d3a763c52e479d3318733a80
snakshay <45660095+snakshay@users.noreply.github.com>
2022-11-14 13:29:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 
-const socket = io.connect('https://upset-dove-outfit.cyclic.app/',{path: "/chat/"});
+const socket = io.connect('https://chatapp-snakshay.herokuapp.com/');
 
 const darkTheme = createTheme(theme);
 
